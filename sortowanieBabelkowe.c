e504f16631b5c4701b46edaa0a6961f1aa759b84
perzan22 <100600167+perzan22@users.noreply.github.com>
2022-03-07 18:09:42 +0100

    Update sortowanieBabelkowe.c

--- sortowanieBabelkowe.c
+++ sortowanieBabelkowe.c
@@ -2,22 +2,22 @@
 
 void sortowanieBabelkowe()
 {
-    int cyfry[] = {6, 7, 2, 9, 0, 2, 5, 8, 1, 6, 9, 4, 5, 6, 3};
-    bubbleSort(cyfry);
-    for (int i = 0; i < (sizeof cyfry / sizeof(int)); i++)
+    int cyfry[] = {6, 7, 2, 9, 0, 2, 5, 8, 1, 6, 9, 4, 5, 6, 3};    /* tworzenie tablicy 15 - elemenetowej */
+    bubbleSort(cyfry);  /* wywołanie funkcji bubbleSort() */
+    for (int i = 0; i < (sizeof cyfry / sizeof(int)); i++)  /* pętla wypisująca elementy tablicy */
     printf("%d ", cyfry[i]);
 }
 
-void bubbleSort(int tab[])
+void bubbleSort(int tab[])  /* deklaracja funkcji bubbleSort */
 
 {
     int temp;
-    for (int i = 0; i < 14; i++) {
-        for (int j = 0; j < 14 - i; j++){
-            if (tab[j] > tab[j + 1]) {
-                temp = tab[j];
-                tab[j] = tab[j + 1];
-                tab[j + 1] = temp;
+    for (int i = 0; i < 14; i++) {  /* pętla for sortująca tablicę, przechodzi przez każdy element tablicy po kolei */
+        for (int j = 0; j < 14 - i; j++){   /* pętla for sprawdzająca element z kolejnym elementem */   
+            if (tab[j] > tab[j + 1]) {  /* sprawdzenie czy element jest większy od kolejnego */
+                temp = tab[j];  /* zapisanie elementu do tymczasowej zmiennej */
+                tab[j] = tab[j + 1];    /* zmiana wartości elementu na wartość kolejenego elementu */
+                tab[j + 1] = temp;  /* zastąpienie kolejnego elementu tymczasową zmienną */
             }
         }
     }
