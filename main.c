2d416ce84d9e41d9a4aaa47e7fa6c12c419f6f54
perzan22 <100600167+perzan22@users.noreply.github.com>
2022-03-07 17:48:23 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{
+    int cyfry[] = {6, 7, 2, 9, 0, 2, 5, 8, 1, 6, 9, 4, 5, 6, 3};
+    bubbleSort(cyfry);
+    for (int i = 0; i < (sizeof cyfry / sizeof(int)); i++)
+    printf("%d ", cyfry[i]);
+}
+
+void bubbleSort(int tab[])
+
+{
+    int temp;
+    for (int i = 0; i < 14; i++) {
+        for (int j = 0; j < 14 - i; j++){
+            if (tab[j] > tab[j + 1]) {
+                temp = tab[j];
+                tab[j] = tab[j + 1];
+                tab[j + 1] = temp;
+            }
+        }
+    }
+}
