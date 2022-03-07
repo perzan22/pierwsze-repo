23886f4251c0f8900612cf3db04cbbdf812278c2
perzan22 <100600167+perzan22@users.noreply.github.com>
2022-03-07 17:35:50 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{
+    int cyfry[] = {4, 8, 4, 3, 9, 0, 1, 6, 2, 8, 5, 7, 2, 7, 0};
+    insertionSort(cyfry);
+    for (int i = 0; i < (sizeof cyfry / sizeof(int)); i++)
+    printf("%d ", cyfry[i]);
+}
+
+void insertionSort(int tab[])
+
+{
+    int j, temp;
+    for (int i = 1; i < 15; i++) {
+        temp = tab[i];
+        j = i - 1;
+        while (j >= 0 && tab[j] > temp){
+            tab[j + 1] = tab[j];
+            j--;
+            }
+            tab[j + 1] = temp;
+        }
+}
